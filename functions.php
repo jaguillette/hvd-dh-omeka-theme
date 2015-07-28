56b487017f24410af157a2f0bd0dff380aac80e4
Jeremy Guillette <jaguillette@gmail.com>
2015-07-28 16:08:43 -0400

    added functions for displaying multiple random featured collections/exhibits

---
+++ functions.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Get random featured collections (plural).
+ *
+ * @package Omeka\Function\View\Collection
+ * @uses get_records()
+ * @param integer $num The maximum number of recent items to return
+ * @param boolean|null $hasImage
+ * @return array|Collection
+ */
+function dh_get_random_featured_collections($num = 5, $hasImage = null)
+{
+    return get_records('Collection', array('featured' => 1,
+                                     'sort_field' => 'random',
+                                     'hasImage' => $hasImage), $num);
+}
+/**
+ * Get HTML for random featured collections (plural).
+ *
+ * @package Omeka\Function\View\Collection
+ * @uses dh_get_random_featured_collections()
+ * @param int $count Maximum number of items to show.
+ * @param boolean $withImage Whether or not the featured items must have
+ * images associated. If null, as default, all featured items can appear,
+ * whether or not they have files. If true, only items with files will appear,
+ * and if false, only items without files will appear.
+ * @return string
+ */
+function dh_random_featured_collections($count = 5, $hasImage = null)
+{
+    $collections = dh_get_random_featured_collections($count, $hasImage);
+    if ($collections) {
+        $html = '';
+        foreach ($collections as $collection) {
+            $html .= get_view()->partial('collections/single.php', array('collection' => $collection));
+            release_object($collection);
+        }
+    } else {
+        $html = '<p>' . __('No featured collections are available.') . '</p>';
+    }
+    return $html;
+}
+/**
+ * Get random featured exhibits.
+ *
+ * @package Omeka\Function\View\Exhibit
+ * @uses get_records()
+ * @param integer $num The maximum number of recent exhibits to return
+ * @param boolean|null $hasImage
+ * @return array|Exhibit
+ */
+function dh_get_random_featured_exhibits($num = 5, $hasImage = null)
+{
+    return get_records('Exhibit', array('featured' => 1,
+                                     'sort_field' => 'random',
+                                     'hasImage' => $hasImage), $num);
+}
+/**
+ * Return the HTML for summarizing a random featured exhibit
+ *
+ * @return string
+ */
+function dh_display_random_featured_exhibits($num = 5, $hasImage = null)
+{
+	$html = '<div id="featured-exhibit">';
+	$exhibits = dh_get_random_featured_exhibits($num, $hasImage);
+	$html .= '<h2>' . __('Featured Exhibits') . '</h2>';
+	if ($exhibits) {
+		foreach ($exhibits as $exhibit) {
+			$html .= get_view()->partial('exhibits/single.php', array('exhibit' => $exhibit));
+			release_object($exhibit);
+		}
+	} else {
+		$html .= '<p>' . __('You have no featured exhibits.') . '</p>';
+	}
+    $html .= '</div>';
+    $html = apply_filters('exhibit_builder_display_random_featured_exhibit', $html);
+    return $html;
+}
+?>
--- index.php
+++ index.php
@@ -5,21 +5,7 @@
     <?php endif; ?>
     <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
     <!-- Featured Exhibit -->
-    <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
-    <?php endif; ?>
-    <?php if (get_theme_option('Display Featured Collection')): ?>
-    <!-- Featured Collection -->
-    <div id="featured-collection" class="featured">
-        <h2><?php echo __('Featured Collection'); ?></h2>
-        <?php echo random_featured_collection(); ?>
-    </div><!-- end featured collection -->
-    <?php endif; ?>
-    <?php if (get_theme_option('Display Featured Item') == 1): ?>
-    <!-- Featured Item -->
-    <div id="featured-item" class="featured">
-        <h2><?php echo __('Featured Item'); ?></h2>
-        <?php echo random_featured_items(1); ?>
-    </div><!--end featured-item-->
+    <?php echo dh_display_random_featured_exhibits(5); ?>
     <?php endif; ?>
 
 </div><!-- end primary -->
@@ -39,6 +25,21 @@
         <?php echo recent_items($recentItems); ?>
         <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
     </div><!--end recent-items -->
+    <?php else: ?>
+        <?php if (get_theme_option('Display Featured Collection')): ?>
+        <!-- Featured Collection -->
+        <div id="featured-collection" class="featured">
+            <h2><?php echo __('Featured Collections'); ?></h2>
+            <?php echo dh_random_featured_collections(5); ?>
+        </div><!-- end featured collection -->
+        <?php endif; ?>
+        <?php if (get_theme_option('Display Featured Item') == 1): ?>
+        <!-- Featured Item -->
+        <div id="featured-item" class="featured">
+            <h2><?php echo __('Featured Item'); ?></h2>
+            <?php echo random_featured_items(1); ?>
+        </div><!--end featured-item-->
+        <?php endif; ?>
     <?php endif; ?>
     
     <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
