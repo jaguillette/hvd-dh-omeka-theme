9bbe71bda2208a1191b91b070c6b173b2ff8784f
Jeremy Guillette <jaguillette@gmail.com>
2015-10-22 09:26:42 -0400

    added custom "show" page to remove linear navigation in exhibits"

---
+++ exhibit-builder/exhibits/show.php
@@ -0,0 +1,33 @@
+<?php
+echo head(array(
+    'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
+    'bodyclass' => 'exhibits show'));
+?>
+
+<h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></h1>
+
+<div id="exhibit-blocks">
+<?php exhibit_builder_render_exhibit_page(); ?>
+</div>
+
+<!--<div id="exhibit-page-navigation">
+    <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
+    <div id="exhibit-nav-prev">
+    <?php echo $prevLink; ?>
+    </div>
+    <?php endif; ?>
+    <?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
+    <div id="exhibit-nav-next">
+    <?php echo $nextLink; ?>
+    </div>
+    <?php endif; ?>
+    <div id="exhibit-nav-up">
+    <?php echo exhibit_builder_page_trail(); ?>
+    </div>
+</div>-->
+
+<nav id="exhibit-pages">
+    <h4><?php echo exhibit_builder_link_to_exhibit($exhibit); ?></h4>
+    <?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
+</nav>
+<?php echo foot(); ?>
