7ad9e88991a7d2a23e81d95ee602b5bd6613ab9b
Jeremy Guillette <jaguillette@gmail.com>
2015-11-23 17:21:18 -0500

    removed fancybox from item page

--- items/show.php
+++ items/show.php
@@ -45,27 +45,4 @@ queue_css_file('lightbox');
 
 </div> <!-- End of Primary. -->
 
-<script src="<?php echo(substr(PUBLIC_THEME_DIR,13)); ?>/dh-theme/javascripts/vendor/jquery.fancybox.js"></script>
-<script type="text/javascript">
-var thumbnails = jQuery.map(jQuery('img'), function(element) { return jQuery(element).attr('src'); });
-var fancybox_gallery = jQuery.map(jQuery('img'), function(element) {
-    jElement = jQuery(element);
-    var href = jElement.attr('src');
-    href = href.replace("square_thumbnails","fullsize");
-    var title = jElement.attr('title');
-    var linkOut = jElement.parent().attr('href');
-    title = '<a href="'+linkOut+'">'+title+'</a>';
-    fancybox_item = {"href":href,"title":title};
-    return fancybox_item;
-});
-jQuery("img").parent().click(function(e) {
-    e.preventDefault();
-    var startIndex = thumbnails.indexOf(e.currentTarget.firstElementChild.getAttribute('src'));
-    var imageTitle = e.currentTarget.firstElementChild.getAttribute('title');
-    jQuery.fancybox(fancybox_gallery,{
-        index:startIndex
-    });
-})
-</script>
-
  <?php echo foot(); ?>
