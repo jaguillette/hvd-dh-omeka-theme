b28825584b17f717159dba28be2164e22e7c9ad8
Jeremy Guillette <jaguillette@gmail.com>
2016-02-16 10:48:35 -0500

    put item metadata into inline table

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -3,15 +3,15 @@
     <?php if ($showElementSetHeadings): ?>
     <h2><?php echo html_escape(__($setName)); ?></h2>
     <?php endif; ?>
+    <table class="item-metadata">
     <?php foreach ($setElements as $elementName => $elementInfo): ?>
-    <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
-        <div>
-        <strong><?php echo html_escape(__($elementName)); ?>:</strong>
+    <tr id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
+        <th><?php echo html_escape(__($elementName)); ?>:</th>
         <?php foreach ($elementInfo['texts'] as $text): ?>
-            <span class="element-text"><?php echo $text; ?></span>
+            <td class="element-text"><?php echo $text; ?></td>
         <?php endforeach; ?>
-        </div>
-    </div><!-- end element -->
+    </tr><!-- end element -->
     <?php endforeach; ?>
+    </table>
 </div><!-- end element-set -->
 <?php endforeach;
