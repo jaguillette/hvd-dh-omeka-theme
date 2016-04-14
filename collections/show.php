092f700d03c717467dd726d658121a94caddcca7
Jeremy Guillette <jaguillette@gmail.com>
2016-04-14 15:26:10 -0400

    fix for #12
    Re-worked collections/show to better indicate that it's a front door, and to link to browse items by collection. Items on collections/show now only display as images, to reinforce that this is not always the entire collection

--- collections/show.php
+++ collections/show.php
@@ -6,7 +6,7 @@ $collectionTitle = strip_formatting(metadata('collection', array('Dublin Core', 
 
 <h1><?php echo $collectionTitle; ?></h1>
 
-<?php if ($collectionAttribution = get_theme_option('Collection Attribution')) { ?>
+<?php if ($collectionAttribution = get_theme_option('Collection Attribution')): ?>
 <div id="collection-attribution">
 <?php
   $collectionOwnerName = dh_get_user_by_id($collection['owner_id'])['name'];
@@ -14,7 +14,7 @@ $collectionTitle = strip_formatting(metadata('collection', array('Dublin Core', 
   echo("$collectionAttrLabel $collectionOwnerName");
 ?>
 </div>
-<?php } ?>
+<?php endif; ?>
 
 <?php 
     $description =  metadata('collection', array('Dublin Core','Description'));
@@ -24,26 +24,14 @@ $collectionTitle = strip_formatting(metadata('collection', array('Dublin Core', 
 ?>
 
 <div id="collection-items">
-    <?php if (False): ?><h2><?php echo link_to_items_browse(__('Items in the %s Collection', $collectionTitle), array('collection' => metadata('collection', 'id'))); ?></h2><?php endif;?>
     <?php if (metadata('collection', 'total_items') > 0): ?>
         <?php foreach (loop('items') as $item): ?>
         <?php $itemTitle = strip_formatting(metadata('item', array('Dublin Core', 'Title'))); ?>
-        <div class="item hentry">
-            <h3><?php echo link_to_item($itemTitle, array('class'=>'permalink')); ?></h3>
+        <div class="hentry">
 
             <?php if (metadata('item', 'has thumbnail')): ?>
             <div class="item-img">
                 <?php echo link_to_item(item_image('square_thumbnail', array('alt' => $itemTitle))); ?>
-            </div>
-            <?php endif; ?>
-
-            <?php if ($text = metadata('item', array('Item Type Metadata', 'Text'), array('snippet'=>250))): ?>
-            <div class="item-description">
-                <p><?php echo $text; ?></p>
-            </div>
-            <?php elseif ($description = metadata('item', array('Dublin Core', 'Description'), array('snippet'=>250))): ?>
-            <div class="item-description">
-                <?php echo $description; ?>
             </div>
             <?php endif; ?>
         </div>
@@ -52,6 +40,9 @@ $collectionTitle = strip_formatting(metadata('collection', array('Dublin Core', 
         <p><?php echo __("There are currently no items within this collection."); ?></p>
     <?php endif; ?>
 </div><!-- end collection-items -->
+
+
+<div id="collection-browse-link"><?php echo link_to_items_browse(__('View items in the %s Collection', $collectionTitle), array('collection' => metadata('collection', 'id'))); ?></div>
 
 <?php fire_plugin_hook('public_collections_show', array('view' => $this, 'collection' => $collection)); ?>
 
