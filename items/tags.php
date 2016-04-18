e7c0ad508f315b4d8b3be8601d54d8191c4c6125
Jeremy Guillette <jaguillette@gmail.com>
2016-04-18 15:09:45 -0400

    added multi-tag browsing
    runs on javascript, but allows for selecting multiple tags from browse tags page for items. Work done on #15, may be applicable to other content types.

---
+++ items/tags.php
@@ -0,0 +1,43 @@
+<?php
+$pageTitle = __('Browse Items');
+echo head(array('title'=>$pageTitle, 'bodyclass'=>'items tags'));
+?>
+
+<h1><?php echo $pageTitle; ?></h1>
+
+<nav class="navigation items-nav secondary-nav">
+    <?php echo public_nav_items(); ?>
+</nav>
+
+<?php echo tag_cloud($tags, 'items/browse'); ?>
+
+<div id="multi-tag"></div>
+
+<script type="text/javascript">
+	function insert_checkboxes(element_selector, form_selector) {
+		var items = jQuery(element_selector);
+		items.each( function() {
+			jQuery(this).before('<input type="checkbox" class="multi-tag" value="'+jQuery(this).text()+'">');
+		})
+	}
+
+	insert_checkboxes(".hTagcloud li a");
+
+	var selected_tags = [];
+
+	jQuery('#multi-tag').append('<div id="multi-tag-submit"><a href="#" >Find selected tags</a></div>')
+
+	jQuery(".multi-tag").on('click', function() {
+		if (jQuery(this).is(":checked")) {
+			selected_tags.push(jQuery(this).attr("value"));
+		} else {
+			var index = selected_tags.indexOf(jQuery(this).attr("value"));
+			if (index > -1) {
+				selected_tags.splice(index, 1);
+			}
+		}
+		jQuery("#multi-tag-submit a").attr("href", "browse?tags="+selected_tags.join(","));
+	})
+</script>
+
+<?php echo foot(); ?>
