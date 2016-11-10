9da2d23b2931f0b73cefdd6174f880acbde4dfda
Jeremy Guillette <jguillette@fas.harvard.edu>
2016-11-10 17:57:15 -0500

    improved gallery loading
    Gallery wasn't loading when going from one record to another, but a setTimeout function seems to have resolved it. Also broke down some code into more functions

--- javascripts/fancybox-display.js
+++ javascripts/fancybox-display.js
@@ -1,48 +1,66 @@
-var make_fancybox_gallery = function() {
+var set_thumbnails = function() {
   var thumbnails = jQuery.map(
     jQuery('.download-file img'),
     function(element) { return jQuery(element).attr('src'); }
   );
+  return thumbnails;
+};
+
+var set_gallery = function() {
   var fancybox_gallery = jQuery.map(jQuery('.download-file img'), function(element) {
-      jElement = jQuery(element);
-      // var href = jElement.attr('filename');
-      var href = jElement.attr('src');
-      href = href.replace(/%2F/g,'/');
-      href = href.split('/');
-      href.reverse();
-      href[0] = jElement.attr('filename');
-      href[1] = 'original';
-      href.reverse();
-      href = href.join('/');
-      var title = jElement.attr('title');
-      var linkOut = jElement.parent().attr('href');
-      title = '<a href="'+linkOut+'">'+title+'</a>';
-      fancybox_item = {
-        "href":href,
-        "title":title,
-        "type":"image",
-      };
-      return fancybox_item;
+    jElement = jQuery(element);
+    // var href = jElement.attr('filename');
+    var href = jElement.attr('src');
+    href = href.replace(/%2F/g,'/');
+    href = href.split('/');
+    href.reverse();
+    href[0] = jElement.attr('filename');
+    href[1] = 'original';
+    href.reverse();
+    href = href.join('/');
+    var title = jElement.attr('title');
+    var linkOut = jElement.parent().attr('href');
+    title = '<a href="'+linkOut+'">'+title+'</a>';
+    var fancybox_item = {
+      "href":href,
+      "title":title,
+      "type":"image",
+    };
+    return fancybox_item;
   });
-  setTimeout(function() {
-    jQuery(".download-file").click(function(e) {
-      e.preventDefault();
-      var startIndex = thumbnails.indexOf(e.currentTarget.firstElementChild.getAttribute('src'));
-      var imageTitle = e.currentTarget.firstElementChild.getAttribute('title');
-      jQuery.fancybox(fancybox_gallery,{
-          index:startIndex,
-          afterLoad:function() {
-            this.title = '<a href="'+this.href+'">Fullsize Image</a>  |  ' + this.title;
-          }
-      });
-    })
-  },300)
+  return fancybox_gallery;
+};
+
+var onImageClick = function(e,thumbnails,fancybox_gallery) {
+  e.preventDefault();
+  var startIndex = 0
+  startIndex = thumbnails.indexOf(e.currentTarget.firstElementChild.getAttribute('src'));
+  var imageTitle = e.currentTarget.firstElementChild.getAttribute('title');
+  jQuery.fancybox(fancybox_gallery,{
+    index:startIndex,
+    afterLoad:function() {
+      this.title = '<a href="'+this.href+'">Fullsize Image</a>  |  ' + this.title;
+    }
+  });
+  jQuery.fancybox(fancybox_gallery,{
+      index:startIndex,
+      afterLoad:function() {
+        console.log("afterload");
+        this.title = '<a href="'+this.href+'">Fullsize Image</a>  |  ' + this.title;
+      }
+  });
+};
+
+var make_fancybox_gallery = function() {
+  var thumbnails = set_thumbnails();
+  var fancybox_gallery = set_gallery();
+  jQuery(".download-file").click(function(e) { onImageClick(e,thumbnails,fancybox_gallery); });
 };
 
 make_fancybox_gallery();
 
 if (typeof Neatline !== 'undefined' ) {
   Neatline.vent.on('select', function() {
-    setTimeout(make_fancybox_gallery(), 300);
+    setTimeout(function() { make_fancybox_gallery(); }, 500);
   });
 }
